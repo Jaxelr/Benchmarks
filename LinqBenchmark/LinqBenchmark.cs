d95cb886ab72fd43c2177241232d074e08ab9ae3
Jaxel Rojas <jaxelr@gmail.com>
2020-11-24 10:04:07 -0400

    feat: added more linq methods

--- LinqBenchmark/LinqBenchmark.cs
+++ LinqBenchmark/LinqBenchmark.cs
@@ -1,7 +1,7 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Order;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
 
 namespace LinqBenchmark
 {
@@ -20,6 +20,18 @@ namespace LinqBenchmark
         [ArgumentsSource(nameof(InputData))]
         [Benchmark]
         public bool CountUsage(IEnumerable<int> list, int value) => list.Count(m => m == value) > 0;
+
+        [ArgumentsSource(nameof(InputData))]
+        [Benchmark]
+        public int FirstUsage(IEnumerable<int> list, int value) => list.First(m => m == value);
+
+        [ArgumentsSource(nameof(InputData))]
+        [Benchmark]
+        public int SingleUsage(IEnumerable<int> list, int value) => list.Single(m => m == value);
+
+        [ArgumentsSource(nameof(InputData))]
+        [Benchmark]
+        public List<int> WhereUsage(IEnumerable<int> list, int value) => list.Where(m => m == value).ToList();
 
         public IEnumerable<object[]> InputData()
         {
