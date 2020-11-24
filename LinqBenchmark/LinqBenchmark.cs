d9ce7bbdb0dc2d5e4ec5d48f5d091d5ffe64e567
Jaxel Rojas <jaxelr@gmail.com>
2020-11-24 09:33:33 -0400

    feat: added linqbenchmark proj

---
+++ LinqBenchmark/LinqBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqBenchmark
+{
+    [BenchmarkCategory("Linq")]
+    [CategoriesColumn]
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MarkdownExporterAttribute.GitHub]
+    public class LinqBenchmark
+    {
+        [ArgumentsSource(nameof(InputData))]
+        [Benchmark]
+        public bool AnyUsage(IEnumerable<int> list, int value) => list.Any(m => m == value);
+
+        [ArgumentsSource(nameof(InputData))]
+        [Benchmark]
+        public bool CountUsage(IEnumerable<int> list, int value) => list.Count(m => m == value) > 0;
+
+        public IEnumerable<object[]> InputData()
+        {
+            yield return new object[] { Enumerable.Range(0, 1000), 100 };
+            yield return new object[] { Enumerable.Range(0, 10000), 1000 };
+            yield return new object[] { Enumerable.Range(0, 100000), 10000 };
+        }
+    }
+}
---
+++ LinqBenchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace LinqBenchmark
+{
+    internal static class Program
+    {
+        private static void Main() => _ = BenchmarkRunner.Run<LinqBenchmark>();
+    }
+}
