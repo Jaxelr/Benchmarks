7f5fef33e3ecc9c22098425f2c0199c1dd63e41f
Jaxel Rojas <jaxelr@gmail.com>
2022-04-01 14:19:39 -0400

    feat: initial version of the benchmark of rail fence cipher

---
+++ RailFenceCipherBenchmark/RailFenceBenchmarkLinq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RailFenceCipherBenchmark
+{
+    public class RailFenceCipherLinq
+    {
+        private readonly int rails;
+
+        public RailFenceCipherLinq(int rails) => this.rails = --rails;
+
+        internal int GetRow(int i) => rails - Math.Abs((i % (2 * rails)) - rails);
+
+        internal IEnumerable<T> ZigZagIndex<T>(IEnumerable<T> seq) => seq
+            .Select((c, i) => (c, r: GetRow(i)))
+            .GroupBy(v => v.r)
+            .SelectMany(v => v.Select(vc => vc.c));
+
+        public string Encode(string input) => string.Join("", ZigZagIndex(input));
+
+        public string Decode(string input) =>
+            ZigZagIndex(Enumerable.Range(0, input.Length))
+            .Zip(input, (f, s) => (i: f, c: s))
+            .OrderBy(v => v.i)
+            .Aggregate(new StringBuilder(), (sb, v) => sb.Append(v.c), sb => sb.ToString());
+    }
+}
---
+++ RailFenceCipherBenchmark/RailFenceCipherBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace RailFenceCipherBenchmark
+{
+    [BenchmarkCategory("RailFenceCipher")]
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MarkdownExporterAttribute.GitHub]
+    public class RailFenceBenchmark
+    {
+        private const int N = 30;
+        private readonly RailFenceCipherLinq cipherLinq;
+        private readonly RailFenceCipherLoop cipherLoop;
+
+        public RailFenceBenchmark()
+        {
+            cipherLinq = new(N);
+            cipherLoop = new(N);
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Arrays))]
+        public string RailFenceLinq(string value) => cipherLinq.Encode(value);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Arrays))]
+        public string RailFenceLoop(string value) => cipherLoop.Encode(value);
+
+        public static IEnumerable<object[]> Arrays()
+        {
+            yield return new object[] { new string('*', 500) };
+            yield return new object[] { new string('*', 1000) };
+            yield return new object[] { "Random*String*With*Asterisks*In*Between" };
+        }
+    }
+}
