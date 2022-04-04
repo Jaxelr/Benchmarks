8e055dd8367ace110cda66d3f270ada04aa685f1
Jaxel Rojas <jaxelr@gmail.com>
2022-04-03 22:16:03 -0400

    refactor: added decode ops

--- RailFenceCipherBenchmark/RailFenceCipherBenchmark.cs
+++ RailFenceCipherBenchmark/RailFenceCipherBenchmark.cs
@@ -22,18 +22,33 @@ namespace RailFenceCipherBenchmark
         }
 
         [Benchmark]
-        [ArgumentsSource(nameof(Arrays))]
-        public string RailFenceLinq(string value) => cipherLinq.Encode(value);
+        [ArgumentsSource(nameof(InputStringArrays))]
+        public string RailFenceLinqEncode(string value) => cipherLinq.Encode(value);
 
         [Benchmark]
-        [ArgumentsSource(nameof(Arrays))]
-        public string RailFenceLoop(string value) => cipherLoop.Encode(value);
+        [ArgumentsSource(nameof(InputStringArrays))]
+        public string RailFenceLoopEncode(string value) => cipherLoop.Encode(value);
 
-        public static IEnumerable<object[]> Arrays()
+        [Benchmark]
+        [ArgumentsSource(nameof(OutputStringArrays))]
+        public string RailFenceLinqDecode(string value) => cipherLinq.Decode(value);
+
+        [Benchmark]
+        [ArgumentsSource(nameof(OutputStringArrays))]
+        public string RailFenceLoopDecode(string value) => cipherLoop.Decode(value);
+
+        public static IEnumerable<object[]> InputStringArrays()
         {
             yield return new object[] { new string('*', 500) };
             yield return new object[] { new string('*', 1000) };
             yield return new object[] { "Random*String*With*Asterisks*In*Between" };
+        }
+
+        public static IEnumerable<object[]> OutputStringArrays()
+        {
+            yield return new object[] { new string('*', 500) };
+            yield return new object[] { new string('*', 1000) };
+            yield return new object[] { "Rotgtsi*BeadmSrn*ihAtrssI*ewen*iW*ekntn" };
         }
     }
 }
