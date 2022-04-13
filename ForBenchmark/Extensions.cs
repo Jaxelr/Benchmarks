13d0aaf250fd47518b55b01be24a1b9fb1139ca0
Jaxel Rojas <jaxelr@gmail.com>
2022-04-13 08:25:53 -0400

    style: supressed roslynator suggestion
    style: added formatting

--- ForBenchmark/Extensions.cs
+++ ForBenchmark/Extensions.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ForBenchmark
 {
     internal static class Extensions
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1224:Make method an extension method.", Justification = "<Pending>")]
         internal static IEnumerable<int> CustomRange(int from, int to, int inc)
         {
             for (int i = from; i <= to; i += inc)
@@ -13,17 +13,17 @@ namespace ForBenchmark
         }
 
         internal static RangeEnumerator GetEnumerator(this Range @this) => (@this.Start, @this.End) switch
-    {
-        ({ IsFromEnd: true, Value: 0 }, { IsFromEnd: true, Value: 0 }) => new RangeEnumerator(0, int.MaxValue, 1),
-        ({ IsFromEnd: true, Value: 0 }, { IsFromEnd: false, Value: var to }) => new RangeEnumerator(0, to + 1, 1),
-        ({ IsFromEnd: false, Value: var from }, { IsFromEnd: true, Value: 0 }) => new RangeEnumerator(from, int.MaxValue, 1),
-        ({ IsFromEnd: false, Value: var from }, { IsFromEnd: false, Value: var to })
-            => (from < to) switch
-            {
-                true => new RangeEnumerator(from, to, 1),
-                false => new RangeEnumerator(from, to, -1),
-            },
-        _ => throw new InvalidOperationException("Invalid range")
-    };
+        {
+            ({ IsFromEnd: true, Value: 0 }, { IsFromEnd: true, Value: 0 }) => new RangeEnumerator(0, int.MaxValue, 1),
+            ({ IsFromEnd: true, Value: 0 }, { IsFromEnd: false, Value: var to }) => new RangeEnumerator(0, to + 1, 1),
+            ({ IsFromEnd: false, Value: var from }, { IsFromEnd: true, Value: 0 }) => new RangeEnumerator(from, int.MaxValue, 1),
+            ({ IsFromEnd: false, Value: var from }, { IsFromEnd: false, Value: var to })
+                => (from < to) switch
+                {
+                    true => new RangeEnumerator(from, to, 1),
+                    false => new RangeEnumerator(from, to, -1),
+                },
+            _ => throw new InvalidOperationException("Invalid range")
+        };
     }
 }
