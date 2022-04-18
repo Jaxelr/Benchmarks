39ce73b177728611f3c1a76f65150cda225911b2
Jaxel Rojas <jaxelr@gmail.com>
2022-04-18 11:00:40 -0400

    refactor: update the append benchmarks with the new config

--- AppendBenchmark/AppendBenchmark.cs
+++ AppendBenchmark/AppendBenchmark.cs
@@ -6,6 +6,7 @@ using System.Linq;
 namespace AppendBenchmark
 {
     [BenchmarkCategory("Append")]
+    [AllStatisticsColumn]
     [MemoryDiagnoser]
     [ShortRunJob]
     [Orderer(SummaryOrderPolicy.FastestToSlowest)]
